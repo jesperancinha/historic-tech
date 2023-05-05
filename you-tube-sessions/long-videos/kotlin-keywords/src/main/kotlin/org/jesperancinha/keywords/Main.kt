2186656a33ed8177964c2da6bcf7191088bde2b9
Joao Esperancinha <jofisaes@gmail.com>
2023-05-05 23:55:44 +0200

    Shifts project location

---
+++ you-tube-sessions/long-videos/kotlin-keywords/src/main/kotlin/org/jesperancinha/keywords/Main.kt
@@ -0,0 +1,36 @@
+package org.jesperancinha.keywords
+
+/**
+ * inline
+ * in
+ * out
+ * this
+ * init
+ * receiver
+ * actual
+ * annotation
+ * crossinline
+ * companion
+ * external
+ * expect
+ * infix
+ * inline
+ * inner
+ * internal
+ * lateinit
+ * noinline
+ * open
+ * operator
+ * reified
+ * sealed
+ * suspend
+ * tailrec
+ * vararg
+ */
+fun main(args: Array<String>) {
+    println("Hello World!")
+
+    // Try adding program arguments via Run/Debug configuration.
+    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
+    println("Program arguments: ${args.joinToString()}")
+}
