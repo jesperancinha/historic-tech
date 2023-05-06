a4a028738c6bb3732a6823e06f44c9269ca76042
Joao Esperancinha <jofisaes@gmail.com>
2023-05-06 21:48:09 +0200

    Makes Contravariant example

--- you-tube-sessions/jeorg-long-videos/jeorg-kotlin-keywords/src/main/kotlin/org/jesperancinha/keywords/episodes/Episode1.kt
+++ you-tube-sessions/jeorg-long-videos/jeorg-kotlin-keywords/src/main/kotlin/org/jesperancinha/keywords/episodes/Episode1.kt
@@ -2,8 +2,27 @@ package org.jesperancinha.keywords.episodes
 
 import org.slf4j.LoggerFactory
 
-
 val logger: org.slf4j.Logger = LoggerFactory.getLogger(Episode1::class.java)
+
+open class Account
+
+class DebitAccount : Account()
+
+class CreditAccount : Account()
+
+open class Money
+
+open class Note : Money()
+
+open class Coin : Money()
+
+class Card<out A : Account, in M : Money>(
+    private val account: A
+) {
+    fun addMoney( money: Money) = account
+}
+
+
 /**
  * inline
  * in
@@ -16,6 +35,23 @@ class Episode1 {
     companion object {
         @JvmStatic
         fun main(args: Array<String>) {
+            runInlineExample()
+            runContravariantExample()
+            runCovariantExample()
+        }
+
+        private fun runCovariantExample() {
+            val account = DebitAccount()
+            val debitCard = Card<DebitAccount, Money>(account)
+            val creditAccount = CreditAccount()
+            val creditCard = Card<CreditAccount, Money> (creditAccount)
+        }
+
+        private fun runContravariantExample() {
+        }
+
+
+        private fun runInlineExample() {
             processMortgage()
             processMortgage()
         }
