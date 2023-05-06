f65e984ce477cee52273550b8879dee3a2cc1426
Joao Esperancinha <jofisaes@gmail.com>
2023-05-06 19:06:36 +0200

    creates logger

--- you-tube-sessions/jeorg-long-videos/jeorg-kotlin-keywords/src/main/kotlin/org/jesperancinha/keywords/episodes/Episode1.kt
+++ you-tube-sessions/jeorg-long-videos/jeorg-kotlin-keywords/src/main/kotlin/org/jesperancinha/keywords/episodes/Episode1.kt
@@ -1,9 +1,10 @@
 package org.jesperancinha.keywords.episodes
 
+import org.slf4j.LoggerFactory
 import java.util.logging.Logger
 
 
-val logger: Logger = LoggerFactory
+val logger: org.slf4j.Logger = LoggerFactory.getLogger(Episode1::class.java)
 /**
  * inline
  * in
@@ -15,4 +16,6 @@ val logger: Logger = LoggerFactory
 class Episode1 {
 }
 
-inline fun processMortgage() {}
+inline fun processMortgage() {
+
+}
