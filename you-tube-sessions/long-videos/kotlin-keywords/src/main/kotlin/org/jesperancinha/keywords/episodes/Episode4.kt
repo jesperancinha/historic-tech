695bfddfee657bfe6e68370ed042bc80b2abb64d
Joao Esperancinha <jofisaes@gmail.com>
2023-05-10 20:47:49 +0200

    suspend example

--- you-tube-sessions/long-videos/kotlin-keywords/src/main/kotlin/org/jesperancinha/keywords/episodes/Episode4.kt
+++ you-tube-sessions/long-videos/kotlin-keywords/src/main/kotlin/org/jesperancinha/keywords/episodes/Episode4.kt
@@ -1,14 +1,18 @@
 package org.jesperancinha.keywords.episodes
 
+import kotlinx.coroutines.*
+import java.math.BigDecimal
+import kotlin.system.measureTimeMillis
+
 sealed class BonusCard {
-    object DiscountCard : BonusCard()
+    class DiscountCard(val value: BigDecimal = 10.toBigDecimal()) : BonusCard()
 
     object HalfBonusCard : BonusCard() {
         object DoubleBonusCard : BonusCard()
     }
 
     init {
-        println("${this.javaClass.simpleName.replace("Card","")} Card Created!")
+        println("${this.javaClass.simpleName.replace("Card", "")} Card Created!")
     }
 }
 
@@ -52,13 +56,31 @@ class Episode4 {
                 logger.error("This fails because while we are preserving the type and no error occurs during compilation\nthe runtime fails because an incorrect cast ends up being made")
             }
 
-            BonusCard.DiscountCard
+            BonusCard.DiscountCard()
             BonusCard.HalfBonusCard
             BonusCard.HalfBonusCard.DoubleBonusCard
+
+            runBlocking {
+                val time = measureTimeMillis {
+                    withContext(Dispatchers.IO) {
+                        listOf(
+                            async {
+                                delay(1000)
+                                create10BonusCards()
+                            },
+                            async {
+                                delay(1000)
+                                create10BonusCards()
+                            })
+                    }.awaitAll()
+                }
+                logger.info("It took $time milliseconds to execute!")
+            }
         }
     }
 }
 
+suspend fun create10BonusCards() = (1 until 10).map { BonusCard.DiscountCard() }
 inline fun <reified T : Account> createDoubleNoteAccount(account: T): T = when (account.moneyCollection.size) {
     10 -> DebitAccount(account.name, account.moneyCollection) as T
     20 -> CreditAccount(account.name, account.moneyCollection) as T
