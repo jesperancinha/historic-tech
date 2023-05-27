4f5af927b56be27d666d99fdbbccb69a8ff139d2
Joao Esperancinha <jofisaes@gmail.com>
2023-05-27 22:23:01 +0200

    All spinoff inline explained

--- you-tube-sessions/long-videos/kotlin-keywords/src/main/kotlin/org/jesperancinha/keywords/spinoffs/MagazineReader.kt
+++ you-tube-sessions/long-videos/kotlin-keywords/src/main/kotlin/org/jesperancinha/keywords/spinoffs/MagazineReader.kt
@@ -2,6 +2,9 @@ package org.jesperancinha.keywords.spinoffs
 
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
+import java.lang.Thread.sleep
+
+private const val SLEEP_TIME = 700L
 
 class MagazineReader {
     companion object {
@@ -9,7 +12,7 @@ class MagazineReader {
         val logger:Logger = LoggerFactory.getLogger(MagazineReader::class.java)
         @JvmStatic
         fun main(args: Array<String>) {
-            val magazine1 = """
+            val magazine = """
                 There are many articles in this magazine.
                 Some are good and some are bad.
                 Some are great and some are greater.
@@ -18,14 +21,52 @@ class MagazineReader {
                 - Article2: Maine Coon only likes chique hair dresser
                 After several tries this Maine Coon only found this hair dresser to be acceptable. It is the most expensive hairdresser in town.
             """.trimIndent()
-            val article1 = findFavouriteArticle(magazine1)
-            val article2 = findFavouriteArticle(magazine1)
+            val articleInline1 = findFavouriteArticleInline(magazine)
+            logger.info("Favourite inline article1: $articleInline1")
+            val articleInline2 = findFavouriteArticleInline(magazine)
+            logger.info("Favourite inline article2: $articleInline2")
+            val article1 = findFavouriteArticle(magazine)
             logger.info("Favourite article1: $article1")
+            val article2 = findFavouriteArticle(magazine)
             logger.info("Favourite article2: $article2")
+            val articleInline1WithHelp = findFavouriteArticleInlineWithHelp(magazine){
+                substring(0, 10)
+            }
+            logger.info("Favourite inline article1: $articleInline1WithHelp")
+            val articleInline2WithHelp = findFavouriteArticleInlineWithHelp(magazine){
+                substring(0, 10)
+            }
+            logger.info("Favourite inline article2: $articleInline2WithHelp")
+            val article1WithHelp = findFavouriteArticleWithHelp(magazine){
+                substring(0, 20)
+            }
+            logger.info("Favourite article1: $article1WithHelp")
+            val article2WithHelp = findFavouriteArticleWithHelp(magazine){
+                substring(0, 20)
+            }
+            logger.info("Favourite article2: $article2WithHelp")
         }
 
-        inline private fun findFavouriteArticle(magazine: String)
+        private inline fun findFavouriteArticleInline(magazine: String)
         =  magazine.split("Article").takeLast(2)
             .map { it.split(":")[1].trim() }.random()
+            .apply { sleep(SLEEP_TIME) }
+
+        private fun findFavouriteArticle(magazine: String)
+        =  magazine.split("Article").takeLast(2)
+            .map { it.split(":")[1].trim() }.random()
+            .apply { sleep(SLEEP_TIME) }
+
+        private inline fun findFavouriteArticleInlineWithHelp(magazine: String,transform: String.()->String= {this})
+        =  magazine.split("Article").takeLast(2)
+            .map { it.split(":")[1].trim() }.random()
+            .let { transform(it) }
+            .apply { sleep(SLEEP_TIME) }
+
+        private fun findFavouriteArticleWithHelp(magazine: String, transform: String.()->String= {this})
+        =  magazine.split("Article").takeLast(2)
+            .map { it.split(":")[1].trim() }.random()
+            .let { transform(it) }
+            .apply { sleep(SLEEP_TIME) }
     }
 }
