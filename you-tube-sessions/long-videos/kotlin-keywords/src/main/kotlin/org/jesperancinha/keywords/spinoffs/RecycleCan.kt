8aa1f54ab3a7b9d21c290717015001e9f3d92001
Joao Esperancinha <jofisaes@gmail.com>
2023-06-05 07:35:57 +0200

    Expands crossinline example

--- you-tube-sessions/long-videos/kotlin-keywords/src/main/kotlin/org/jesperancinha/keywords/spinoffs/RecycleCan.kt
+++ you-tube-sessions/long-videos/kotlin-keywords/src/main/kotlin/org/jesperancinha/keywords/spinoffs/RecycleCan.kt
@@ -2,23 +2,41 @@ package org.jesperancinha.keywords.spinoffs
 
 
 fun main() {
-    recycleCan {
+    val nonLocalReturn = recycleCan {
         println("Use the shredder")
+        println("Give a sign that you are done!")
+        recycleCanCompletion {
+            (1..10).forEach {
+                if (it == 2) return@forEach
+                println("Shipped can $it")
+            }
+            return@recycleCanCompletion
+        }
         return@recycleCan
     }
-    recycleCanAndForget {
+    val noNonLocalReturn = recycleCanAndForget {
         println("Use the shredder and use materials for other things")
+        println("But you don't have to return anything")
+        println("But you don't have to return anything")
         // This is not possible because crossinline prevents non-local returns
         // Why is this called crossinline?
         // And why do we need to avoid them?
         // Inline is mandatory because crossinline changes the bytecode of the function.
-        // return@recycleCan
+        recycleCanCompletion {
+            (1..10).forEach {
+                if (it == 2) return@forEach
+                println("Shipped can $it")
+            }
+            return@recycleCanCompletion
+        }
+//        return@recycleCan
     }
+
+    println(nonLocalReturn)
+    println(noNonLocalReturn)
 }
 
-fun recycleCan( recycleFunction: ()-> Unit) {
-    recycleFunction()
-}
-inline fun recycleCanAndForget(crossinline recycleFunction: ()-> Unit) {
-    recycleFunction()
-}
+inline fun recycleCan(recycleFunction: () -> Unit) = recycleFunction()
+fun recycleCanCompletion(recycleFunction: () -> Unit) = recycleFunction()
+
+inline fun recycleCanAndForget(crossinline recycleFunction: () -> Unit) = recycleFunction()
