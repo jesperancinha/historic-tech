8cb9dc898a2ee7d562032e01b128000012b8314d
Joao Esperancinha <jofisaes@gmail.com>
2023-06-09 14:21:43 +0200

    Code improvements and abstract example

--- you-tube-sessions/long-videos/drinks-manager/src/main/kotlin/Main.kt
+++ you-tube-sessions/long-videos/drinks-manager/src/main/kotlin/Main.kt
@@ -1,40 +1,34 @@
 import java.util.UUID
 
-open class Drink()
+open class Drink
 
-open class WarmDrink() : Drink()
+open class WarmDrink : Drink()
 
-open class ColdDrink() : Drink()
+open class ColdDrink : Drink()
 
-open class TeaDrink() : WarmDrink()
+open class TeaDrink : WarmDrink()
 
-open class CoffeeDrink() : WarmDrink()
+open class CoffeeDrink : WarmDrink()
 
-open class Lemonade() : ColdDrink()
+open class Lemonade : ColdDrink()
 
-open class OrangeJuice() : ColdDrink()
+open class OrangeJuice : ColdDrink()
 
-open class Box()
+open class Box
 
-open class CardboardBox() : Box()
+open class CardboardBox : Box()
 
-open class PlasticBox() : Box()
+open class PlasticBox : Box()
 
-open class FamilyBox(drink: Drink) : CardboardBox()
+open class FamilyBox(val drink: Drink) : CardboardBox()
 
-open class DeluxeBox(drink: Drink) : PlasticBox()
+open class DeluxeBox(val drink: Drink) : PlasticBox()
 
 open class DrinksService<in DRINK : Drink, out BOX : Box> {
     private val database by lazy { HashMap<UUID, Drink>() }
 
     fun sendDrink(drink: DRINK) = run { database[UUID.randomUUID()] = drink }
 
-    /**
-     * We force a cast to BOX for the purpose of the example
-     * Ideally this would return a BOX out of the database or perhaps
-     * Making the class abstract would be a better solution
-     */
-    @Suppress("UNCHECKED_CAST")
     fun getBox(createBox: (Drink) -> Box): BOX = createBox(
         requireNotNull(database.remove(database.keys.last()))) as BOX
 }
@@ -55,18 +49,19 @@ fun main(args: Array<String>) {
     coldDrinksService.getBox { FamilyBox(it) }
     coldDrinksService.getBox { FamilyBox(it) }
 
-    val warmeDrinksService = DrinksService<WarmDrink, DeluxeBox>()
-    warmeDrinksService.sendDrink(CoffeeDrink())
-    warmeDrinksService.sendDrink(TeaDrink())
-    warmeDrinksService.getBox { DeluxeBox(it) }
-    warmeDrinksService.getBox { DeluxeBox(it) }
+    val warmDrinkDeluxeBoxDrinksService = DrinksService<WarmDrink, DeluxeBox>()
+    warmDrinkDeluxeBoxDrinksService.sendDrink(CoffeeDrink())
+    warmDrinkDeluxeBoxDrinksService.sendDrink(TeaDrink())
+    warmDrinkDeluxeBoxDrinksService.getBox { DeluxeBox(it) }
+    warmDrinkDeluxeBoxDrinksService.getBox { DeluxeBox(it) }
 
     val genericDrinksService = DrinksService<Drink, CardboardBox>()
 
-    checkResistenceState(warmeDrinksService)
+    checkResistenceState(warmDrinkDeluxeBoxDrinksService)
     checkCoolerState(coldDrinksService)
     checkResistenceState(genericDrinksService)
     checkCoolerState(genericDrinksService)
 
     DrinksManagerJava.main(args)
+    org.jesperancinha.asngiasagf.variance.main(args)
 }
---
+++ you-tube-sessions/long-videos/drinks-manager/src/main/kotlin/org/jesperancinha/asngiasagf/variance/MainWithAbstract.kt
@@ -0,0 +1,78 @@
+package org.jesperancinha.asngiasagf.variance
+
+import java.util.UUID
+
+open class Drink
+
+open class WarmDrink : Drink()
+
+open class ColdDrink : Drink()
+
+open class TeaDrink : WarmDrink()
+
+open class CoffeeDrink : WarmDrink()
+
+open class Lemonade : ColdDrink()
+
+open class OrangeJuice : ColdDrink()
+
+open class Box
+
+open class CardboardBox : Box()
+
+open class PlasticBox : Box()
+
+open class FamilyBox(val drink: Drink) : CardboardBox()
+
+open class DeluxeBox(val drink: Drink) : PlasticBox()
+
+abstract class DrinksService<in DRINK : Drink, out BOX : Box> {
+    protected val database by lazy { HashMap<UUID, Drink>() }
+
+    fun sendDrink(drink: DRINK) = run { database[UUID.randomUUID()] = drink }
+
+    abstract fun getBox(): BOX
+}
+
+open class ColdDrinksService : DrinksService<ColdDrink, FamilyBox>() {
+    override fun getBox(): FamilyBox = FamilyBox(requireNotNull(database.remove(database.keys.last())))
+}
+
+open class WarmDrinksService : DrinksService<WarmDrink, DeluxeBox>() {
+    override fun getBox(): DeluxeBox = DeluxeBox(requireNotNull(database.remove(database.keys.last())))
+}
+open class GenericDrinksService : DrinksService<Drink, DeluxeBox>() {
+    override fun getBox(): DeluxeBox = DeluxeBox(requireNotNull(database.remove(database.keys.last())))
+}
+
+
+fun checkResistenceState(drinksService: DrinksService<WarmDrink, Box>) {
+
+}
+
+fun checkCoolerState(drinksService: DrinksService<ColdDrink, Box>) {
+
+}
+
+fun main(args: Array<String>) {
+    val coldDrinksService = ColdDrinksService()
+    coldDrinksService.sendDrink(Lemonade())
+    coldDrinksService.sendDrink(OrangeJuice())
+    coldDrinksService.getBox()
+    coldDrinksService.getBox()
+
+    val warmDrinksService = WarmDrinksService()
+    warmDrinksService.sendDrink(CoffeeDrink())
+    warmDrinksService.sendDrink(TeaDrink())
+    warmDrinksService.getBox()
+    warmDrinksService.getBox()
+
+    val genericDrinksService = GenericDrinksService()
+
+    checkResistenceState(warmDrinksService)
+    checkCoolerState(coldDrinksService)
+    checkResistenceState(genericDrinksService)
+    checkCoolerState(genericDrinksService)
+
+    DrinksManagerJava.main(args)
+}
