74755b98817227a51582efef9d289e345a6dfef4
Joao Esperancinha <jofisaes@gmail.com>
2023-07-04 14:53:49 +0200

    Creates pure info data class

--- experiments/annotations-demo/src/main/kotlin/org/jesperancinha/annotationsdemo/AnnotationsDemoApplication.kt
+++ experiments/annotations-demo/src/main/kotlin/org/jesperancinha/annotationsdemo/AnnotationsDemoApplication.kt
@@ -1,7 +1,10 @@
 package org.jesperancinha.annotationsdemo
 
+import com.fasterxml.jackson.annotation.JsonProperty
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.web.bind.annotation.PathVariable
+import java.math.BigDecimal
 
 @SpringBootApplication
 open class AnnotationsDemoApplication
@@ -9,3 +12,13 @@ open class AnnotationsDemoApplication
 fun main(args: Array<String>) {
 	runApplication<AnnotationsDemoApplication>(*args).start()
 }
+
+data class PureInfo(
+	val title: String,
+	val author: String,
+	val vat: BigDecimal,
+	val ratePerHour: BigDecimal,
+	val ratePerWeek: BigDecimal,
+	val ratePerMonth: BigDecimal,
+	val isGood: Boolean
+)
