81324ac419c48fcc66af341f54679b9768b10c57
Joao Esperancinha <jofisaes@gmail.com>
2023-07-29 07:56:52 +0200

    Adds example

--- you-tube-sessions/long-videos/streams/src/main/kotlin/org/jesperancinha/streams/StreamsHotColdMain.kt
+++ you-tube-sessions/long-videos/streams/src/main/kotlin/org/jesperancinha/streams/StreamsHotColdMain.kt
@@ -1,4 +1,39 @@
 package org.jesperancinha.streams
-fun main() {
 
+import kotlinx.coroutines.*
+import kotlinx.coroutines.channels.Channel
+import java.lang.Math.random
+
+fun main() {
+    runChannelExample()
+    Thread.sleep(1000)
+    runChannelCloseExample()
+    Thread.sleep(1000)
+}
+
+private fun runChannelCloseExample() {
+    CoroutineScope(Dispatchers.IO).launch {
+        val channel = Channel<Int>()
+        launch {
+            repeat(10) {
+                channel.send((random() * 123).toInt())
+            }
+            channel.close()
+        }
+        for (y in channel) println(y)
+        println("Complete!")
+    }
+}
+
+private fun runChannelExample() {
+    CoroutineScope(Dispatchers.IO).launch {
+        val channel = Channel<Int>()
+        launch {
+            repeat(10) {
+                channel.send((random() * 123).toInt())
+            }
+        }
+        repeat(10) { println(channel.receive()) }
+        println("Complete!")
+    }
 }
