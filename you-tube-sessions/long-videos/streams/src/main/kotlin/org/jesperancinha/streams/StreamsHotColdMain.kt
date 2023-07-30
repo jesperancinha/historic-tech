3eeb6350bf0638f73a403e91b69cdab5f1433f5a
Joao Esperancinha <jofisaes@gmail.com>
2023-07-30 11:41:03 +0200

    Producer channel example

--- you-tube-sessions/long-videos/streams/src/main/kotlin/org/jesperancinha/streams/StreamsHotColdMain.kt
+++ you-tube-sessions/long-videos/streams/src/main/kotlin/org/jesperancinha/streams/StreamsHotColdMain.kt
@@ -2,23 +2,35 @@ package org.jesperancinha.streams
 
 import kotlinx.coroutines.*
 import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.channels.ReceiveChannel
+import kotlinx.coroutines.channels.consumeEach
+import kotlinx.coroutines.channels.produce
 import java.lang.Math.random
 
 fun main() {
     runChannelExample()
-    Thread.sleep(1000)
+    pause()
     runChannelCloseExample()
     Thread.sleep(1000)
 }
+
+private const val NUMBERS_FOR_EXAMPLE = 10
 
 private fun runChannelCloseExample() {
     CoroutineScope(Dispatchers.IO).launch {
         val channel = Channel<Int>()
         launch {
-            repeat(10) {
+            repeat(NUMBERS_FOR_EXAMPLE) {
                 channel.send((random() * 123).toInt())
             }
             channel.close()
+            runCatching {
+                repeat(NUMBERS_FOR_EXAMPLE) {
+                    channel.send((random() * 123).toInt())
+                }
+            }.onFailure {
+                println("Failed to send an extra of $NUMBERS_FOR_EXAMPLE numbers because of ${it.javaClass}")
+            }
         }
         for (y in channel) println(y)
         println("Complete!")
@@ -29,11 +41,11 @@ private fun runChannelExample() {
     CoroutineScope(Dispatchers.IO).launch {
         val channel = Channel<Int>()
         launch {
-            repeat(10) {
+            repeat(NUMBERS_FOR_EXAMPLE) {
                 channel.send((random() * 123).toInt())
             }
         }
-        repeat(10) { println(channel.receive()) }
+        repeat(NUMBERS_FOR_EXAMPLE) { println(channel.receive()) }
         println("Complete!")
     }
 }
