e1871e75847d007eae8a68d005c64c8906263b0b
Joao Esperancinha <jofisaes@gmail.com>
2023-08-04 11:21:55 +0200

    Enables Scheduling

--- you-tube-sessions/long-videos/library-channels/src/main/kotlin/org/jesperancinha/library/librarychannels/LibraryChannelsApplication.kt
+++ you-tube-sessions/long-videos/library-channels/src/main/kotlin/org/jesperancinha/library/librarychannels/LibraryChannelsApplication.kt
@@ -2,8 +2,10 @@ package org.jesperancinha.library.librarychannels
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.scheduling.annotation.EnableScheduling
 
 @SpringBootApplication
+@EnableScheduling
 class LibraryChannelsApplication
 
 fun main(args: Array<String>) {
--- you-tube-sessions/long-videos/library-channels/src/main/kotlin/org/jesperancinha/library/librarychannels/service/BookService.kt
+++ you-tube-sessions/long-videos/library-channels/src/main/kotlin/org/jesperancinha/library/librarychannels/service/BookService.kt
@@ -20,21 +20,22 @@ class BookService {
     }
 
     @Scheduled(cron = "*/1 * * * *")
-    fun scheduleStats() = CoroutineScope(Dispatchers.IO).launch{
-        repeat(10){
-          launch {
-              resultChannel.send(statsChannel.receive().book.pages)
-          }
+    fun scheduleStats() {
+        CoroutineScope(Dispatchers.IO).launch {
+            repeat(10) {
+                launch {
+                    resultChannel.send(statsChannel.receive().book.pages)
+                }
+            }
+            val totalPageCount = resultChannel.toList().sum()
+            println("We just received all reservations sent to the channel at one point in time. This is known as a Fan-Out")
+            println("We did send data from multiple coroutines, which makes this also a Fan-In")
+            println("This is in fact a mixed operation: ")
+            println("Several coroutines send data to one channel (Fan-In))")
+            println("Several coroutines receive data from one channel (Fan-Out")
+            println("Once the data is receive, another channel receives all the data from the multiple coroutines that sent the individual number of pages")
+            println("This last operation is known as a Fan-In also")
+            println("The result is $totalPageCount pages!")
         }
-        val totalPageCount = resultChannel.toList().sum()
-        println("We just received all reservations sent to the channel at one point in time. This is known as a Fan-Out")
-        println("We did send data from multiple coroutines, which makes this also a Fan-In")
-        println("This is in fact a mixed operation: ")
-        println("Several coroutines send data to one channel (Fan-In))")
-        println("Several coroutines receive data from one channel (Fan-Out")
-        println("Once the data is receive, another channel receives all the data from the multiple coroutines that sent the individual number of pages")
-        println("This last operation is known as a Fan-In also")
-        println("The result is $totalPageCount pages!")
-
     }
 }
