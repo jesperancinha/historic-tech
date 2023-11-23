2c7f8e08915a7bd4f6cea14b180465ede225df41
Joao Esperancinha <jofisaes@gmail.com>
2023-11-23 20:33:32 +0100

    cleanup

--- you-tube-sessions/long-videos/k2-compiler/benchmark.sh
+++ you-tube-sessions/long-videos/k2-compiler/benchmark.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/bash
 
+echo "Iteration, Regular compiler (s) , K2 compiler (s)" > stats.csv
+
+
+for i in {0..10}
+do
+
 rm -r build
 cp build.gradle.original.txt build.gradle.kts
 start=`date +%s.%N`
@@ -8,8 +14,6 @@ end=`date +%s.%N`
 runtimek2=$( echo "$end - $start" | bc -l )
 echo $runtimek2
 
-
-read love
 
 
 rm -r build
@@ -22,4 +26,6 @@ end=`date +%s.%N`
 runtime=$( echo "$end - $start" | bc -l )
 echo $runtime
 
-echo "$runtimek2 - $runtime"
+echo "$i , $runtimek2 , $runtime" >> stats.csv
+
+done;
