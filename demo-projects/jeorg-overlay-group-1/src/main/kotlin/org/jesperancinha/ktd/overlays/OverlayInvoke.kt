36e69cfaa8536f8f0e2ab975786329afe2328553
jesperancinha <jofisaes@gmail.com>
2024-02-25 21:01:17 +0100

    Extends bank example

--- demo-projects/jeorg-overlay-group-1/src/main/kotlin/org/jesperancinha/ktd/overlays/OverlayInvoke.kt
+++ demo-projects/jeorg-overlay-group-1/src/main/kotlin/org/jesperancinha/ktd/overlays/OverlayInvoke.kt
@@ -1,14 +1,28 @@
 package org.jesperancinha.ktd.overlays
 
 class Bank {
+
+    private val name: String
+
+    constructor() {
+        this.name = "Default bank name"
+    }
+
+    constructor(name: String) {
+        this.name = name
+    }
+
     companion object {
         operator fun invoke() = println("Welcome to this great bank!")
         operator fun invoke(message: String) = println("The bank welcomes you with $message")
+        operator fun invoke(message: String, times: Long) = println("The bank welcomes you with $message $times times")
 
         @JvmStatic
         fun main(args: Array<String> = emptyArray()) {
-            Bank()
-            Bank("bankruptcy")
+            println(invoke())
+            println(Bank())
+            println(Bank("bankruptcy"))
+            println(Bank("bankruptcy", 2))
             println("Can we make a ${Bank()}?")
         }
     }
