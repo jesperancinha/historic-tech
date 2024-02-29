c986375aaa632e844da09bf7de606683b76c3d8d
jesperancinha <jofisaes@gmail.com>
2024-02-29 12:58:39 +0100

    Code refactoring

--- cobol-projects/factorial-recursive.cob
+++ cobol-projects/factorial-recursive.cob
@@ -4,17 +4,17 @@
        DATA DIVISION.
        WORKING-STORAGE SECTION.
 
-         77 n pic 99.
-         77 i pic 99.
-         77 tnumber pic XX.
-         77 fact pic 9(18) comp.
-         77 factst pic X(18).
+         77 n PIC 99.
+         77 i PIC 99.
+         77 tnumber PIC XX.
+         77 fact PIC 9(18) comp.
+         77 factst PIC X(18).
 
        PROCEDURE DIVISION.
          MOVE 0 to i
          MOVE 1 to fact
          MOVE 20 to n
-         PERFORM until i greater than n
+         PERFORM until i GREATER THAN n
            MOVE i to tnumber
            MOVE fact to factst
            DISPLAY "Factorial of " tnumber " or " tnumber "! = " factst
