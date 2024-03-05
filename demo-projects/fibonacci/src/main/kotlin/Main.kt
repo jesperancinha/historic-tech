64fa1aedb1f35d118918c5d0692e5c3e5cdd2364
jesperancinha <jofisaes@gmail.com>
2024-03-05 19:54:38 +0100

    Refactoring

---
+++ demo-projects/fibonacci/src/main/kotlin/Main.kt
@@ -0,0 +1,52 @@
+package org.jesperancinha
+
+import org.jesperancinha.Main.Companion.fibonacciRecursive
+
+
+class Main {
+    companion object {
+        @JvmStatic
+        fun main(args: Array<String> = emptyArray()) {
+            println(fibonacciIterative(100))
+            println(fibonacciTailRecursiveTCO(100))
+            println(fibonacciTailRecursive(100))
+        }
+
+        fun fibonacciIterative(n: Int): Double {
+            if (n <= 1) {
+                return n.toDouble()
+            }
+
+            var a = 0.0
+            var b = 1.0
+
+            for (i in 2..n) {
+                val temp = a + b
+                a = b
+                b = temp
+            }
+
+            return b
+        }
+
+        fun fibonacciRecursive(n: Int): Double {
+            return if (n <= 1) n.toDouble() else fibonacciRecursive(n - 1) + fibonacciRecursive(n - 2)
+        }
+
+        tailrec fun fibonacciTailRecursiveTCO(n: Int, a: Double = 0.0, b: Double = 1.0): Double = if (n == 0) a
+        else fibonacciTailRecursiveTCO(n - 1, b, a + b)
+
+        fun fibonacciTailRecursive(n: Int, a: Double = 0.0, b: Double = 1.0): Double = if (n == 0) a
+        else fibonacciTailRecursive(n - 1, b, a + b)
+    }
+}
+
+
+class MainSlow {
+    companion object {
+        @JvmStatic
+        fun main(args: Array<String>) {
+            println(fibonacciRecursive(100))
+        }
+    }
+}
---
+++ demo-projects/fibonacci/src/test/kotlin/MainKtTest.kt
@@ -0,0 +1,10 @@
+import org.jesperancinha.Main
+import org.junit.jupiter.api.Test
+
+class MainKtTest {
+
+    @Test
+    fun main() {
+       Main.main()
+    }
+}
