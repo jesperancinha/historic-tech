e40411d6a11d9da461e90ce29245137ab36ea0f8
jesperancinha <jofisaes@gmail.com>
2024-03-05 16:18:34 +0100

    Recursive in COBOL

--- cobol-projects/fibonacci-recursive.cob
+++ cobol-projects/fibonacci-recursive.cob
@@ -1,19 +1,27 @@
 IDENTIFICATION DIVISION.
-PROGRAM-ID. Fibonacci.
+PROGRAM-ID. FibonacciRecursive.
+
 DATA DIVISION.
 WORKING-STORAGE SECTION.
 01  F1                   PIC 9(21)V9(1) VALUE 0.0.
 01  F2                   PIC 9(21)V9(1) VALUE 1.0.
 01  FIB                  PIC 9(21)V9(1).
 01  N                    PIC 9(5) VALUE 100.
-01  I                    PIC 9(5) VALUE 3.
+01  I                    PIC 9(5) VALUE 0.
 
 PROCEDURE DIVISION.
-MAIN-LOGIC.
-   PERFORM VARYING I FROM 2 BY 1 UNTIL I > N
+    MOVE N TO I
+    CALL 'FIBONACCI'.
+    DISPLAY "Fibonacci of " I " is " F1.
+    STOP RUN.
+
+    ENTRY 'FIBONACCI'.
+    IF N = 0 THEN
+        EXIT PROGRAM
+    ELSE
        COMPUTE FIB = F1 + F2
        COMPUTE F1 = F2
        COMPUTE F2 = FIB
-   END-PERFORM.
-   DISPLAY FIB.
-   STOP RUN.
+       SUBTRACT 1 FROM N
+       CALL 'FIBONACCI'
+    END-IF.
