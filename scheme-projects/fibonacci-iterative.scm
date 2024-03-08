e306dcc07a4f78b7762a011841266901bfcd23a4
jesperancinha <jofisaes@gmail.com>
2024-03-08 07:59:17 +0100

    Corrects iterative version

--- scheme-projects/fibonacci-iterative.scm
+++ scheme-projects/fibonacci-iterative.scm
@@ -1,7 +1,12 @@
 (define (fibonacci n)
-  (define (fib-iter a b count)
-    (if (= count 0)
-      b
-      (fib-iter (+ a b) a (- count 1))))
-  (fib-iter 1 0 n))
+  (fibonacci-iter n 0 1))
+
+(define (fibonacci-iter n a b)
+  (do ((i n (- i 1))
+        (x a b)
+        (y b (+ a b)))
+    ((= i 0) x)
+    (set! a x)
+    (set! b y)))
+
 (display (fibonacci 100))
