2065a0d54a17b0b508ebac17c17c47d22c850a29
Joao Esperancinha <jofisaes@gmail.com>
2024-06-06 19:53:14 +0200

    HighTech skeleton added

---
+++ demo-projects/guessing-retries/src/main/kotlin/HighTechGameRetry.kt
@@ -0,0 +1,8 @@
+package org.jesperancinha.asnsei.guessing
+
+class HighTechGameRetry(guessingService: CommonGuessingGameService) : Retry<Result<GameOutput>>(guessingService) {
+    override fun checkNumber(inputNumber: Long): Result<GameOutput> {
+        TODO("Not yet implemented")
+    }
+
+}
