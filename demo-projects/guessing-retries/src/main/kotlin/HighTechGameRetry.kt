c294064eb48eb90bb3bb1426d94530f6ac4fab8d
jesperancinha <jofisaes@gmail.com>
2024-06-06 20:41:12 +0200

    HighTechGameRetry

--- demo-projects/guessing-retries/src/main/kotlin/HighTechGameRetry.kt
+++ demo-projects/guessing-retries/src/main/kotlin/HighTechGameRetry.kt
@@ -1,10 +1,43 @@
 package org.jesperancinha.asnsei.guessing
 
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.runBlocking
+import java.util.concurrent.atomic.AtomicLong
+import kotlin.time.Duration.Companion.milliseconds
 
 class HighTechGameRetry(guessingService: CommonGuessingGameService) : Retry<Result<GameOutput>>(guessingService) {
-    override fun checkNumber(inputNumber: Long): Result<GameOutput> = runBlocking{
-        TODO("Not yet implemented")
+    override fun checkNumber(inputNumber: Long): Result<GameOutput> = runBlocking {
+        runCatching(AtomicLong(maxAttempts), inputNumber) {
+            guessingService.checkIfRandomNumberMatchesInput(inputNumber)
+        }
+    }
+    private suspend fun runCatching(
+        atomicAccountRecord: AtomicLong,
+        inputNumber: Long,
+        f: () -> Any
+    ): Result<GameOutput> {
+        atomicAccountRecord.decrementAndGet()
+        return runCatching {
+            f()
+        }.onFailure { ex ->
+            atomicAccountRecord.takeIf { it.get() == 0L }?.let {
+            } ?: delay(delayMillis.milliseconds).also {
+                runCatching(atomicAccountRecord, inputNumber, f)
+            }
+        }.run {
+            if (isFailure) {
+                Result.failure(
+                    GameFailedException(
+                        GameOutput.failedAttempts(
+                            inputNumber,
+                            maxAttempts - atomicAccountRecord.get()
+                        )
+                    )
+                )
+            } else {
+                Result.success(GameOutput.success(inputNumber, maxAttempts - atomicAccountRecord.get()))
+            }
+        }
     }
 
 }
---
+++ demo-projects/guessing-retries/src/test/kotlin/HighTechGameRetryTest.kt
@@ -0,0 +1,36 @@
+
+
+import io.kotest.matchers.booleans.shouldBeTrue
+import io.kotest.matchers.nulls.shouldNotBeNull
+import io.kotest.matchers.shouldBe
+import org.jesperancinha.asnsei.guessing.CommonGuessingGameService
+import org.jesperancinha.asnsei.guessing.GameFailedException
+import org.jesperancinha.asnsei.guessing.HighTechGameRetry
+import org.junit.jupiter.api.Test
+
+class HighTechGameRetryTest {
+    val game by lazy {
+        HighTechGameRetry(object : CommonGuessingGameService() {
+            override fun assertResult(inputNumber: Long): Boolean {
+                return inputNumber == 100L
+            }
+
+        })
+    }
+
+    @Test
+    fun `should resolve correctly 3 times when number fails resulting in fail`() {
+        val checkNumber = game.checkNumber(200)
+        checkNumber.isFailure.shouldBeTrue()
+        val gameResult = (checkNumber.exceptionOrNull() as GameFailedException).gameResult
+        gameResult.retries shouldBe 3
+    }
+
+    @Test
+    fun `should resolve correctly at first try`() {
+        val checkNumber = game.checkNumber(100)
+        checkNumber.isSuccess.shouldBeTrue()
+        val gameResult = checkNumber.getOrNull().shouldNotBeNull()
+        gameResult.retries shouldBe 1
+    }
+}
