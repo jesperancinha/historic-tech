38dc99ef638247033c59061f8e9febbac689b870
jesperancinha <jofisaes@gmail.com>
2024-06-06 20:19:41 +0200

    Fix

--- demo-projects/guessing-retries/src/main/kotlin/HighTechGameRetry.kt
+++ demo-projects/guessing-retries/src/main/kotlin/HighTechGameRetry.kt
@@ -1,7 +1,9 @@
 package org.jesperancinha.asnsei.guessing
 
+import kotlinx.coroutines.runBlocking
+
 class HighTechGameRetry(guessingService: CommonGuessingGameService) : Retry<Result<GameOutput>>(guessingService) {
-    override fun checkNumber(inputNumber: Long): Result<GameOutput> = runBlo{
+    override fun checkNumber(inputNumber: Long): Result<GameOutput> = runBlocking{
         TODO("Not yet implemented")
     }
 
