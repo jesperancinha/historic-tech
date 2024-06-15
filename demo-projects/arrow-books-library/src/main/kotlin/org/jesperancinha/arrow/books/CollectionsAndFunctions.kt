8f4afa50c5c4bb656411e50b89ac11272e2cfcaf
Joao Esperancinha <jofisaes@gmail.com>
2024-06-15 10:24:18 +0200

    42 example for memoization

--- demo-projects/arrow-books-library/src/main/kotlin/org/jesperancinha/arrow/books/CollectionsAndFunctions.kt
+++ demo-projects/arrow-books-library/src/main/kotlin/org/jesperancinha/arrow/books/CollectionsAndFunctions.kt
@@ -1,22 +1,26 @@
 package org.jesperancinha.arrow.books
 
 import arrow.core.memoize
+import kotlin.math.sqrt
 
 class CollectionsAndFunctions {
     companion object {
-        fun expensive(x: Int): Int {
-            Thread.sleep(x * 1000L)
-            return x
+        fun squareRootOf(input: Int): Int {
+            val result = sqrt(input.toDouble()).toInt()
+            Thread.sleep(1000L)
+            return result
         }
 
-        val memoizedExpensive = ::expensive.memoize()
+        val memoizeSquarerootOf = ::squareRootOf.memoize()
 
         @JvmStatic
         fun main(args: Array<String> = emptyArray()) {
-            val result1 = memoizedExpensive(3)
-            val result2 = memoizedExpensive(3)
+            val result1 = memoizeSquarerootOf(1764)
+            val result2 = memoizeSquarerootOf(1764)
+            val result3 = memoizeSquarerootOf(memoizeSquarerootOf(3111696))
             println(result1)
             println(result2)
+            println(result3)
         }
     }
 }
--- demo-projects/arrow-books-library/src/test/kotlin/org/jesperancinha/arrow/books/CollectionsAndFunctionsTest.kt
+++ demo-projects/arrow-books-library/src/test/kotlin/org/jesperancinha/arrow/books/CollectionsAndFunctionsTest.kt
@@ -2,20 +2,28 @@ package org.jesperancinha.arrow.books
 
 import io.kotest.matchers.longs.shouldBeBetween
 import io.kotest.matchers.shouldBe
-import org.jesperancinha.arrow.books.CollectionsAndFunctions.Companion.memoizedExpensive
+import org.jesperancinha.arrow.books.CollectionsAndFunctions.Companion.memoizeSquarerootOf
 import org.junit.jupiter.api.Test
 import kotlin.system.measureTimeMillis
 
 class CollectionsAndFunctionsTest {
     @Test
-    fun `should perform around 1 second`() {
+    fun `should perform in around 2 seconds`() {
         measureTimeMillis {
-            val result1 = memoizedExpensive(1)
-            val result2 = memoizedExpensive(1)
-            result1 shouldBe result2
+            val result1 = memoizeSquarerootOf(1764)
+            val result2 = memoizeSquarerootOf(1764)
+            val result3 = memoizeSquarerootOf(
+                memoizeSquarerootOf(3111696)
+            )
+            println(result1)
+            println(result2)
+            println(result3)
+            result1 shouldBe 42
+            result2 shouldBe 42
+            result3 shouldBe 42
         }.run {
             println("Operation lasted $this milliseconds.")
-            this.shouldBeBetween(900, 1100)
+            this.shouldBeBetween(1900, 2100)
         }
     }
 }
