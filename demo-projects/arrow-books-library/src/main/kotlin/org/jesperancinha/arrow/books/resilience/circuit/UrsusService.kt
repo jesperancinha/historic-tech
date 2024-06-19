75633c5e41b05005b027a4e6f63cdfb0150d05de
João Esperancinha <jofisaes@gmail.com>
2024-06-19 23:27:26 +0200

    Ursi tursi lursi

--- demo-projects/arrow-books-library/src/main/kotlin/org/jesperancinha/arrow/books/resilience/circuit/UrsusService.kt
+++ demo-projects/arrow-books-library/src/main/kotlin/org/jesperancinha/arrow/books/resilience/circuit/UrsusService.kt
@@ -13,33 +13,45 @@ class UrsusService {
         @JvmStatic
         fun main(args: Array<String>) = runBlocking {
             val circuitBreaker = CircuitBreaker(
-                openingStrategy = CircuitBreaker.OpeningStrategy.Count(2),
+                openingStrategy = CircuitBreaker.OpeningStrategy.Count(1),
                 resetTimeout = 2.seconds,
-                exponentialBackoffFactor = 1.2,
+                exponentialBackoffFactor = 2.0,
                 maxResetTimeout = 60.seconds,
             )
 
-            // normal operation
-            circuitBreaker.protectOrThrow { "I am in Closed: ${circuitBreaker.state()}" }.also(::println)
+            justSayingHello(circuitBreaker)
 
-            // simulate service getting overloaded
+            doubtingOnYourExperience(circuitBreaker)
+            doubtingOnYourExperience(circuitBreaker)
+
+            val either = circuitBreaker
+                .protectEither { println("I am trying to calm down. ARE YOU DOUBTING MY EXPERIENCE?!?!??!? ${circuitBreaker.state()}") }
+            println(either.getOrNone())
+
+            println("Me the Ursus is going to SLEEP! I need to calm down, but I HAVE TESTED EVERYTHING AND EVERYTHING WORKS! ${circuitBreaker.state()}")
+            delay(2000)
+            println("I AM TRYING TO CALM DOWN! ${circuitBreaker.state()}")
+            doubtingOnYourExperience(circuitBreaker)
+            delay(2000)
+            delay(2000)
+            println("I AM TRYING TO CALM DOWN! ${circuitBreaker.state()}")
+
+
+            circuitBreaker.protectOrThrow {
+                "I CANNOT SLEEP! But I can calm down. Or at least PRETEND TO!: ${circuitBreaker.state()}"
+            }.also(::println)
+            println("Finally you accept that I AM THE BEST! ${circuitBreaker.state()}")
+        }
+
+        private suspend fun doubtingOnYourExperience(circuitBreaker: CircuitBreaker) {
             Either.catch {
                 circuitBreaker.protectOrThrow { throw RuntimeException("I have tested it, it works perfectly and I am an angry bear and my code IS THE BEST!") }
             }.also(::println)
-            Either.catch {
-                circuitBreaker.protectOrThrow { throw RuntimeException("Service overloaded") }
-            }.also(::println)
-            circuitBreaker.protectEither { }
-                .also { println("I am Open and short-circuit with ${it}. ${circuitBreaker.state()}") }
+        }
 
-            // simulate reset timeout
-            println("Service recovering . . .").also { delay(2000) }
-
-            // simulate test request success
-            circuitBreaker.protectOrThrow {
-                "I am running test-request in HalfOpen: ${circuitBreaker.state()}"
-            }.also(::println)
-            println("I am back to normal state closed ${circuitBreaker.state()}")
+        private suspend fun justSayingHello(circuitBreaker: CircuitBreaker) {
+            circuitBreaker.protectOrThrow { "Hi there, I am Ursus. Oh my God! are you saying that my code is the best? THANK YOU!: ${circuitBreaker.state()}" }
+                .also(::println)
         }
     }
 }
