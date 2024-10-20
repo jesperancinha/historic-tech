297872ca9a24952769b123bbf354fe20c0cbdb47
João Esperancinha <jofisaes@gmail.com>
2024-10-20 21:39:42 +0200

    Left test

--- demo-projects/arrow-books-library/src/test/kotlin/org/jesperancinha/arrow/books/option/OptionTest.kt
+++ demo-projects/arrow-books-library/src/test/kotlin/org/jesperancinha/arrow/books/option/OptionTest.kt
@@ -1,5 +1,8 @@
+import arrow.core.Either
 import arrow.core.None
+import arrow.core.left
 import arrow.core.none
+import arrow.core.raise.either
 import arrow.core.raise.option
 import io.kotest.matchers.shouldBe
 import org.junit.jupiter.api.Test
@@ -7,10 +10,18 @@ import org.junit.jupiter.api.Test
 class OptionTest {
 
     @Test
-    fun `should test option`(){
+    fun `should test option`() {
         val optionTest = option {
             none<String>().bind()
         }
         optionTest shouldBe None
+    }
+
+    @Test
+    fun `should test left call of Either`() {
+        val optionTest = either {
+            Either.Left("Test").bind()
+        }
+        optionTest shouldBe Either.Left("Test")
     }
 }
