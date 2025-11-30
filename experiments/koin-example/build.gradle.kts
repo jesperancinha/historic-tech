3d2d9b11cc53c48381a2e2125b130c58fc75914b
jesperancinha <11569930+jesperancinha@users.noreply.github.com>
2025-11-30 02:20:00 +0000

    Update java

--- experiments/koin-example/build.gradle.kts
+++ experiments/koin-example/build.gradle.kts
@@ -20,7 +20,7 @@ dependencies {
 }
 
 kotlin {
-    jvmToolchain(24)
+    jvmToolchain(25)
 }
 
 tasks.withType<Test> {
--- master-projects/kotlin/coroutines-masters/build.gradle.kts
+++ master-projects/kotlin/coroutines-masters/build.gradle.kts
@@ -18,5 +18,5 @@ tasks.test {
     useJUnitPlatform()
 }
 kotlin {
-    jvmToolchain(24)
+    jvmToolchain(25)
 }
